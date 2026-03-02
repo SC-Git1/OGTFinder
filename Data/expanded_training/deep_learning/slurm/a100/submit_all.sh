dd88c55c7c1523584c962a2099d583a725f7ae01
LegallyOverworked <jaldert.francois@student.kuleuven.be>
2026-03-02 11:15:46 +0100

    HPC job slurms deep learning

---
+++ Data/expanded_training/deep_learning/slurm/a100/submit_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Submit all jobs for a100
+
+sbatch reg_attention_mlp.slurm
+sbatch reg_baseline_mlp.slurm
+sbatch reg_danets.slurm
+sbatch reg_ft_transformer.slurm
+sbatch reg_gandalf.slurm
+sbatch reg_gated_mlp.slurm
+sbatch reg_grownet.slurm
+sbatch reg_hopular.slurm
+sbatch reg_lassonet.slurm
+sbatch reg_node.slurm
+sbatch reg_realmlp.slurm
+sbatch reg_resnet_mlp.slurm
+sbatch reg_rtdl_resnet.slurm
+sbatch reg_saint.slurm
+sbatch reg_snn.slurm
+sbatch reg_sparse_mlp.slurm
+sbatch reg_tabm.slurm
+sbatch reg_tabnet.slurm
+sbatch reg_tabnet_inspired.slurm
+sbatch reg_tabr.slurm
+sbatch reg_vime.slurm
+sbatch reg_wide_deep.slurm
---
+++ Data/expanded_training/deep_learning/slurm/v100/README.txt
@@ -0,0 +1,8 @@
+SLURM job scripts for deep_learning regression models.
+
+Partition: gpu_v100
+Cluster: genius
+GPUs: 1
+
+Before running, create logfiles directory on the cluster:
+  mkdir -p /scratch/leuven/331/vsc33189/projects/ogtfinder/deep_learning/logfiles
